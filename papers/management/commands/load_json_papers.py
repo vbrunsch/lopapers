8df0ccc647b611af745c54cf8becfb1b93e6d906
vbrunsch <62330755+vbrunsch@users.noreply.github.com>
2024-04-02 03:10:47 -0400

    Update loading function for url and requirements

--- papers/management/commands/load_json_papers.py
+++ papers/management/commands/load_json_papers.py
@@ -3,6 +3,7 @@ import django
 import pandas as pd
 import json
 import re
+import requests
 from django.core.management.base import BaseCommand, CommandError
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "soop.settings")
@@ -35,32 +36,34 @@ def clean_authors(author_string):
 
 
 class Command(BaseCommand):
-    help = 'Loads papers from a JSON file into the database'
+    help = 'Loads papers from a JSON file URL into the database'
 
     def add_arguments(self, parser):
-        parser.add_argument('json_file', type=str, help='Path to the JSON file containing the papers data')
+        parser.add_argument('json_url', type=str, help='URL to the JSON file containing the papers data')
 
     def handle(self, *args, **kwargs):
-        json_file_path = kwargs['json_file']
-        try:
-            with open(json_file_path, 'r', encoding='utf-8') as file:
-                papers_data = json.load(file)
-                for paper_data in papers_data:
-                    # Create the Paper instance
-                    paper_instance = Paper.objects.create(
-                        pmid=paper_data['pmid'],
-                        title=paper_data['title_e'],
-                        authors=clean_authors(paper_data.get('authors', '')),
-                        abstract=paper_data.get('abstract', ''),
-                        year=paper_data.get('year'),
-                        doi=paper_data.get('doi')
-                    )
-                    # Process and add categories
-                    categories = clean_and_split_categories(paper_data.get('assigned_subjects1', ''))
-                    for category_name in categories:
-                        category, _ = Tag.objects.get_or_create(name=category_name)
-                        paper_instance.categories.add(category)  
+        json_url = kwargs['json_url']
+        response = requests.get(json_url)
+        
+        if response.status_code != 200:
+            raise CommandError(f'Failed to fetch data from "{json_url}"')
 
-                self.stdout.write(self.style.SUCCESS(f'Successfully loaded papers from {json_file_path}'))
-        except FileNotFoundError:
-            raise CommandError(f'File "{json_file_path}" does not exist.')
+        papers_data = response.json()
+
+        for paper_data in papers_data:
+            # Create the Paper instance
+            paper_instance = Paper.objects.create(
+                pmid=paper_data['pmid'],
+                title=paper_data['title_e'],
+                authors=clean_authors(paper_data.get('authors', '')),
+                abstract=paper_data.get('abstract', ''),
+                year=paper_data.get('year'),
+                doi=paper_data.get('doi')
+            )
+            # Process and add categories
+            categories = clean_and_split_categories(paper_data.get('assigned_subjects1', ''))
+            for category_name in categories:
+                category, _ = Tag.objects.get_or_create(name=category_name)
+                paper_instance.categories.add(category)  
+
+        self.stdout.write(self.style.SUCCESS(f'Successfully loaded papers from {json_url}'))
